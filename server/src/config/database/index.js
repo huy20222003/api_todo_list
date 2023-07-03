8d48396ed25a5fdbee896dde12f2400c55ad452d
huy20222003 <120671495+huy20222003@users.noreply.github.com>
2023-07-03 16:54:51 +0700

    fix security

--- server/src/config/database/index.js
+++ server/src/config/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
    try {
-    await mongoose.connect('mongodb://127.0.0.1/todo_list', {
+    await mongoose.connect('yourStringConnect', {
       useNewUrlParser: true,
     });
     console.log('Connect sucessfully');
