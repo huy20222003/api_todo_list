f1713b17325fa1eb5f3fb37e3cd08da68730dac9
huy20222003 <huy20222003@gmail.com>
2023-07-20 07:50:56 +0700

    update

--- server/src/index.js
+++ server/src/index.js
@@ -8,8 +8,8 @@ const cors = require('cors');
 app.use(cors());
 app.options('*', cors());
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({limit: '100mb'}));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 //router
 const route = require('./routes');
