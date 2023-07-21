81e1f7bb972416bf0cec6adf6493fcb076994e0e
huy20222003 <120671495+huy20222003@users.noreply.github.com>
2023-07-21 15:38:01 +0700

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -4,15 +4,14 @@ const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
-
-app.use(express.json({limit: '100mb'}));
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 //router
 const route = require('./routes');
 
 //morgan
-app.use(morgan('combine'));
+app.use(morgan('dev')); // Use 'dev' instead of 'combine'
 
 //connect database
 const database = require('./config/database');
