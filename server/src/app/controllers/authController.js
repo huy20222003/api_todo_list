9ab63d18e5b7b3062114d4a89801ba68e9ff0f8d
huy20222003 <huy20222003@gmail.com>
2023-07-24 23:20:13 +0700

    update

--- server/src/app/controllers/authController.js
+++ server/src/app/controllers/authController.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const Users = require('../models/Users');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
@@ -28,7 +29,7 @@ class authController {
                 return res.status(400).json({ status: false, message: 'Username or email already exists!' });
             }else {
                 const newUser = new Users({ fullName, username, email, password });
-                const accessToken = jwt.sign({ newUser }, 'TOKEN_SECRET', {expiresIn: '24h'});
+                const accessToken = jwt.sign({ newUser }, process.env.TOKEN_SECRET, {expiresIn: '24h'});
                 await newUser.save();
                 return res.status(200).json({ status: true, message: 'Register successful!', accessToken });
             }
@@ -55,7 +56,7 @@ class authController {
             if (!passwordMatch) {
                 return res.status(401).json({ status: false, message: 'Invalid username or password!' });
             }
-            const accessToken = jwt.sign({ user }, 'TOKEN_SECRET', {expiresIn: '24h'});
+            const accessToken = jwt.sign({ user }, process.env.TOKEN_SECRET, {expiresIn: '24h'});
             res.status(201).json({ status: true, message: 'Logged successfully! ', accessToken });
         } catch(error) {
             res.status(500).json({ status: false, message: error });
--- server/src/index.js
+++ server/src/index.js
@@ -1,26 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const PORT = process.env.PORT || 3000;
 const cors = require('cors');
 
-
-app.use(express.json({limit: '100mb'}));
+app.use(express.json({ limit: '100mb' }));
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 
 //router
 const route = require('./routes');
 
 //morgan
-app.use(morgan('combine'));
+app.use(morgan('dev')); 
 
 //connect database
 const database = require('./config/database');
 database.connect();
 
 //cors
-app.use(cors());
-app.options('*', cors());
+const corsOptions = {
+  origin: 'https://todolist-webapp-v1.netlify.app/', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+  allowedHeaders: ['Content-Type', 'Authorization'] 
+};
+app.use(cors(corsOptions));
+// app.options('*', cors());
 
 //router
 route(app);
