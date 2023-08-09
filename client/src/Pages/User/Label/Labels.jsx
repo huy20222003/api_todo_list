8a75a6db980b75fe51517ce4d4adee0435bc7016
huy20222003 <120671495+huy20222003@users.noreply.github.com>
2023-08-09 18:56:38 +0700

    Remove back button

--- client/src/Pages/User/Label/Labels.jsx
+++ client/src/Pages/User/Label/Labels.jsx
@@ -12,20 +12,12 @@ import styles from './Labels.module.css';
 const Labels = () => {
   const { setShowAddLabel } = useContext(LabelsContext);
 
-  const handleGoBack = () => {
-    history.back();
-  };
-
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <HeaderContent />
       </div>
       <div className={styles.content}>
-        <div className={styles.backButtonContainer} onClick={handleGoBack}>
-          <i className={`fa-solid fa-arrow-left ${styles.backButton}`}></i>
-          <span className={styles.backButtonDescription}>Back</span>
-        </div>
         <div className={styles.labelList}>
           <Suspense fallback={<Loader />}>
             <LabelItem />
